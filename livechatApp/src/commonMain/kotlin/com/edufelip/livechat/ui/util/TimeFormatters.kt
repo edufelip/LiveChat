d9e45bf3afb7d3913055f027ceb840176277a37e
root <root@DESKTOP-RRAPSB1.localdomain>
2025-11-08 10:58:31 -0300

    fix: address compose warnings

--- livechatApp/src/commonMain/kotlin/com/edufelip/livechat/ui/util/TimeFormatters.kt
+++ livechatApp/src/commonMain/kotlin/com/edufelip/livechat/ui/util/TimeFormatters.kt
@@ -1,14 +1,15 @@
 package com.edufelip.livechat.ui.util
 
-import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.toKotlinInstant
 import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 @OptIn(ExperimentalTime::class)
 fun Long.formatAsTime(timeZone: TimeZone = TimeZone.currentSystemDefault()): String =
     runCatching {
-        val instant = Instant.fromEpochMilliseconds(this)
+        val instant = Instant.fromEpochMilliseconds(this).toKotlinInstant()
         val local = instant.toLocalDateTime(timeZone)
         "${local.hour.pad2()}:${local.minute.pad2()}"
     }.getOrDefault("")
