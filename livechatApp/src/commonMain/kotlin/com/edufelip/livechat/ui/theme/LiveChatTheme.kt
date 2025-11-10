6a3b2c9409d7d03eb4dc2e947696e9b10ac3910c
EduardoSantos <eduardofelipi@gmail.com>
2025-11-10 13:34:59 -0300

    core: Updating color scheme

--- livechatApp/src/commonMain/kotlin/com/edufelip/livechat/ui/theme/LiveChatTheme.kt
+++ livechatApp/src/commonMain/kotlin/com/edufelip/livechat/ui/theme/LiveChatTheme.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.remember
 import com.edufelip.livechat.ui.resources.LiveChatStrings
 import com.edufelip.livechat.ui.resources.LocalLiveChatStrings
 
@@ -16,10 +15,7 @@ fun LiveChatTheme(
     strings: LiveChatStrings = LiveChatStrings(),
     content: @Composable () -> Unit,
 ) {
-    val fallbackStrategy = remember { PastelColorSchemeStrategy() }
-    val baseColors = fallbackStrategy.scheme(darkTheme, palette)
-    val colorScheme =
-        strategy.scheme(darkTheme, palette).copy(surfaceTint = baseColors.surfaceTint)
+    val colorScheme = strategy.scheme(darkTheme, palette)
 
     CompositionLocalProvider(
         LocalLiveChatSpacing provides LiveChatSpacing(),
