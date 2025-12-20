cb65c6eab6eb44fe36034f067a7b949a001674f6
EduSantos <eduardofelipi@gmail.com>
2025-12-20 16:34:58 -0300

    feat(ui): add platform context helper

---
+++ composeApp/src/commonMain/kotlin/com/edufelip/livechat/ui/platform/PlatformContext.kt
@@ -0,0 +1,6 @@
+package com.edufelip.livechat.ui.platform
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun rememberPlatformContext(): Any?
---
+++ composeApp/src/iosMain/kotlin/com/edufelip/livechat/ui/platform/PlatformContext.ios.kt
@@ -0,0 +1,6 @@
+package com.edufelip.livechat.ui.platform
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun rememberPlatformContext(): Any? = null
