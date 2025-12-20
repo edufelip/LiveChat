325634e9d92514c468cf79c135a4d0ae799a4807
EduSantos <eduardofelipi@gmail.com>
2025-12-20 16:29:55 -0300

    chore(ios): configure Firebase SPM

--- iosApp/LiveChatIOS/AppDelegate.swift
+++ iosApp/LiveChatIOS/AppDelegate.swift
@@ -1,7 +1,21 @@
 import UIKit
 import LiveChatCompose
+#if canImport(FirebaseCore)
+import FirebaseCore
+#endif
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     // Scene-based lifecycle; window setup lives in SceneDelegate
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        #if canImport(FirebaseCore)
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+        #endif
+        return true
+    }
 }
