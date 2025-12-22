3c8d13ffbb94c9e731b12012bffee6f4618c7905
EduSantos <eduardofelipi@gmail.com>
2025-12-22 12:03:00 -0300

    Localize Android UI test assertions

--- composeApp/src/androidInstrumentedTest/kotlin/com/edufelip/livechat/ui/features/contacts/ContactsScreenTest.kt
+++ composeApp/src/androidInstrumentedTest/kotlin/com/edufelip/livechat/ui/features/contacts/ContactsScreenTest.kt
@@ -1,12 +1,14 @@
 package com.edufelip.livechat.ui.features.contacts
 
 import androidx.activity.ComponentActivity
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import com.edufelip.livechat.preview.PreviewFixtures
 import com.edufelip.livechat.ui.features.contacts.screens.ContactsScreen
+import com.edufelip.livechat.ui.resources.liveChatStrings
 import com.edufelip.livechat.ui.theme.LiveChatTheme
 import org.junit.Assert.assertTrue
 import org.junit.Rule
@@ -18,8 +20,17 @@ class ContactsScreenTest {
 
     @Test
     fun showsRegisteredAndInviteSections() {
+        var registeredLabel = ""
+        var inviteSectionTitle = ""
+        var inviteCta = ""
         composeRule.setContent {
             LiveChatTheme {
+                val strings = liveChatStrings().contacts
+                SideEffect {
+                    registeredLabel = strings.registeredSectionTitle
+                    inviteSectionTitle = strings.inviteSectionTitle
+                    inviteCta = strings.inviteCta
+                }
                 ContactsScreen(
                     state = PreviewFixtures.contactsState,
                     onInvite = {},
@@ -30,10 +41,11 @@ class ContactsScreenTest {
             }
         }
 
-        val registeredBadges = composeRule.onAllNodesWithText("On LiveChat").fetchSemanticsNodes()
+        composeRule.waitForIdle()
+        val registeredBadges = composeRule.onAllNodesWithText(registeredLabel).fetchSemanticsNodes()
         assertTrue(registeredBadges.isNotEmpty())
-        composeRule.onNodeWithText("Invite to LiveChat").assertIsDisplayed()
-        val inviteButtons = composeRule.onAllNodesWithText("Invite").fetchSemanticsNodes()
+        composeRule.onNodeWithText(inviteSectionTitle).assertIsDisplayed()
+        val inviteButtons = composeRule.onAllNodesWithText(inviteCta).fetchSemanticsNodes()
         assertTrue(inviteButtons.isNotEmpty())
         composeRule.onNodeWithText("Ava Harper").assertIsDisplayed()
         composeRule.onNodeWithText("Chioma Ade").assertIsDisplayed()
