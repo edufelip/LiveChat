3a8b32c6fbc1dbae35764ea8703dc441930e6e58
EduSantos <eduardofelipi@gmail.com>
2025-12-22 15:42:36 -0300

    Add inbox action model to local messages data

---
+++ shared/data/src/commonMain/kotlin/com/edufelip/livechat/data/models/InboxItem.kt
@@ -0,0 +1,23 @@
+package com.edufelip.livechat.data.models
+
+import com.edufelip.livechat.domain.models.Message
+
+enum class InboxActionType {
+    DELIVERED,
+    READ,
+}
+
+data class InboxAction(
+    val id: String,
+    val messageId: String,
+    val senderId: String,
+    val receiverId: String,
+    val actionType: InboxActionType,
+    val actionAtMillis: Long,
+)
+
+sealed class InboxItem {
+    data class MessageItem(val message: Message) : InboxItem()
+
+    data class ActionItem(val action: InboxAction) : InboxItem()
+}
