da253de9820d9c0a15571c8436d293d156312896
EduSantos <eduardofelipi@gmail.com>
2025-12-26 20:56:51 -0300

    feature: fetch iOS contacts with permissions

--- composeApp/src/iosMain/kotlin/com/edufelip/livechat/contacts/ContactsPermissionManager.ios.kt
+++ composeApp/src/iosMain/kotlin/com/edufelip/livechat/contacts/ContactsPermissionManager.ios.kt
@@ -2,26 +2,67 @@ package com.edufelip.livechat.contacts
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import kotlin.coroutines.resume
+import kotlinx.coroutines.suspendCancellableCoroutine
+import platform.Contacts.CNAuthorizationStatusAuthorized
+import platform.Contacts.CNAuthorizationStatusDenied
+import platform.Contacts.CNAuthorizationStatusNotDetermined
+import platform.Contacts.CNAuthorizationStatusRestricted
+import platform.Contacts.CNContactStore
+import platform.Contacts.CNEntityType
 import platform.Foundation.NSProcessInfo
 import platform.Foundation.NSUserDefaults
 
 private class IosContactsPermissionManager : ContactsPermissionManager {
+    private val store = CNContactStore()
     private var currentState: PermissionState =
         when {
             isContactsUiTestDenied() -> PermissionState.Denied
             isContactsUiTestFlowEnabled() -> PermissionState.Denied
-            else -> PermissionState.Granted
+            else -> authorizationStatusToState()
         }
 
     override suspend fun ensurePermission(): PermissionState {
-        if (currentState is PermissionState.Granted || isContactsUiTestDenied()) {
+        if (isContactsUiTestDenied()) {
+            currentState = PermissionState.Denied
             return currentState
         }
-        currentState = PermissionState.Granted
+        if (isContactsUiTestFlowEnabled()) {
+            currentState = PermissionState.Denied
+            return currentState
+        }
+        currentState =
+            when (CNContactStore.authorizationStatusForEntityType(CNEntityType.CNEntityTypeContacts)) {
+                CNAuthorizationStatusAuthorized -> PermissionState.Granted
+                CNAuthorizationStatusDenied, CNAuthorizationStatusRestricted -> PermissionState.Denied
+                CNAuthorizationStatusNotDetermined -> {
+                    if (requestAccess()) PermissionState.Granted else PermissionState.Denied
+                }
+                else -> PermissionState.Denied
+            }
         return currentState
     }
 
     override fun status(): PermissionState = currentState
+
+    private fun authorizationStatusToState(): PermissionState {
+        return when (CNContactStore.authorizationStatusForEntityType(CNEntityType.CNEntityTypeContacts)) {
+            CNAuthorizationStatusAuthorized -> PermissionState.Granted
+            CNAuthorizationStatusDenied, CNAuthorizationStatusRestricted -> PermissionState.Denied
+            CNAuthorizationStatusNotDetermined -> PermissionState.Denied
+            else -> PermissionState.Denied
+        }
+    }
+
+    private suspend fun requestAccess(): Boolean {
+        return suspendCancellableCoroutine { continuation ->
+            store.requestAccessForEntityType(CNEntityType.CNEntityTypeContacts) { granted, _ ->
+                if (continuation.isActive) {
+                    continuation.resume(granted)
+                }
+            }
+        }
+    }
 }
 
 @Composable
