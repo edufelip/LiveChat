5a6e478e10b6a82765649da1daeac18b5caab078
EduSantos <eduardofelipi@gmail.com>
2025-12-27 18:18:04 -0300

    fix: remove duplicate storage bucket config

--- shared/data/src/androidMain/kotlin/com/edufelip/livechat/data/remote/StorageBucketConfig.android.kt
+++
@@ -1,5 +0,0 @@
-package com.edufelip.livechat.data.remote
-
-import com.edufelip.livechat.shared.data.BuildConfig
-
-actual val STORAGE_BUCKET_URL: String = BuildConfig.STORAGE_BUCKET_URL
--- shared/data/src/commonMain/kotlin/com/edufelip/livechat/data/remote/StorageBucketConfig.kt
+++
@@ -1,3 +0,0 @@
-package com.edufelip.livechat.data.remote
-
-expect val STORAGE_BUCKET_URL: String
