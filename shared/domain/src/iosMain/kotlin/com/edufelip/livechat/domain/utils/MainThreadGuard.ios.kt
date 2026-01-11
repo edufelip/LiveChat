5ebf5a5c9ad918aff304e1ca89dd6566592d8790
EduSantos <eduardofelipi@gmail.com>
2026-01-11 11:56:21 -0300

    ci: fixing issue with build compose frameworks step

--- shared/domain/src/iosMain/kotlin/com/edufelip/livechat/domain/utils/MainThreadGuard.ios.kt
+++ shared/domain/src/iosMain/kotlin/com/edufelip/livechat/domain/utils/MainThreadGuard.ios.kt
@@ -1,16 +1,18 @@
 package com.edufelip.livechat.domain.utils
 
 import platform.Foundation.NSThread
-import kotlin.native.concurrent.AtomicInt
+import kotlin.concurrent.atomics.AtomicInt
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
 
 internal actual fun isMainThread(): Boolean = NSThread.isMainThread
 
+@OptIn(ExperimentalAtomicApi::class)
 internal actual class AtomicFlag actual constructor(initial: Boolean) {
     private val atomic = AtomicInt(if (initial) 1 else 0)
 
-    actual fun get(): Boolean = atomic.value != 0
+    actual fun get(): Boolean = atomic.load() != 0
 
     actual fun set(value: Boolean) {
-        atomic.value = if (value) 1 else 0
+        atomic.store(if (value) 1 else 0)
     }
 }
